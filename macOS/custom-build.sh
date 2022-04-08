5915e71a91e2c64dd7dcf90035317433bb938171
Will Iverson <wiverson@gmail.com>
2022-04-08 15:32:12 -0700

    Add R Tree support

--- macOS/custom-build.sh
+++ macOS/custom-build.sh
@@ -3,7 +3,7 @@ mkdir target
 cd target
 tar -xvf ../../sqlite-autoconf.tar.gz 
 cd $(ls)
-CC=clang CFLAGS="-arch x86_64 -arch arm64 -Os -DSQLITE_DQS=0 -DSQLITE_ENABLE_COLUMN_METADATA" ./configure --enable-session
+CC=clang CFLAGS="-arch x86_64 -arch arm64 -Os -DSQLITE_DQS=0 -DSQLITE_ENABLE_COLUMN_METADATA -DSQLITE_ENABLE_RTREE=1" ./configure --enable-session
 make
 cp sqlite3 ../libsqlite3session.dylib
 cd ..
