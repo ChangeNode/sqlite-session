7ab5d93b4019f432a326a644bd3e31d37476cbf5
Will Iverson <wiverson@gmail.com>
2023-04-03 16:01:58 -0700

    Update build2.bat

--- win64/build2.bat
+++ win64/build2.bat
@@ -1,2 +1,2 @@
-cd win64\target\sqlite-amalgamation\sqlite-amalgamation-3380200\
+cd win64\target\sqlite-amalgamation\sqlite-amalgamation-3410200\
 cl -O2 -D SQLITE_API=__declspec(dllexport) -DSQLITE_ENABLE_SESSION -DSQLITE_ENABLE_PREUPDATE_HOOK -DSQLITE_ENABLE_FTS4 -DSQLITE_ENABLE_RTREE=1 -DSQLITE_DQS=0 -DSQLITE_ENABLE_COLUMN_METADATA sqlite3.c -link -dll -out:sqlite3session.dll
